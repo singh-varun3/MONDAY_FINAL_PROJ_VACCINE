8c01dc26e832ecb0384343cec171287d665ebb0f
EthanGomes14 <gomes.eth@northeastern.edu>
2024-12-05 13:05:56 -0500

    Added VaccineCenter Package

--- VaxSphere/src/Transport/TransportDirectory.java
+++ VaxSphere/src/Transport/TransportDirectory.java
@@ -4,10 +4,49 @@
  */
 package Transport;
 
+import Driver.Driver;
+import UserAccount.UserAccount;
+import java.util.ArrayList;
+
 /**
  *
  * @author ethangomes
  */
 public class TransportDirectory {
+    
+    public ArrayList<Transport> getTransportList() {
+        return transportList;
+    }
+
+    public void setTransportList(ArrayList<Transport> transportList) {
+        this.transportList = transportList;
+    }
+    private ArrayList<Transport> transportList;
+    
+    public Transport addNewCar(){
+    Transport car = new Transport();
+    transportList.add(car);
+    return car;
+    }
+    
+    public Transport getRestaurantByAdmin(UserAccount user) {
+        Transport restaurant = null;
+        for (Transport r : transportList) {
+            if(r.getAdmin().equals(user))
+                restaurant = r;
+        }
+        return restaurant;
+    }
+    
+    public Transport getRestaurantByDeliveryMan(UserAccount user) {
+        Transport restaurant = null;
+        for (Transport r : transportList) {
+            for(Driver man: r.getDriverList()) {
+               if(man.getUserAccount().equals(user))
+                restaurant = r; 
+            }  
+        }
+        return restaurant;
+    }
     
 }
