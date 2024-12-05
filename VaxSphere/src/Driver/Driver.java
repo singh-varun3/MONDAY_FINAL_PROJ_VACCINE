afc51a27599880ddb31acd2767e02b0175fc8ab1
Varun Singh <singh.varun3@northeastern.edu>
2024-12-05 12:57:03 -0500

    Created attributes, getter and setters for Driver and Driver Directory

--- VaxSphere/src/Driver/Driver.java
+++ VaxSphere/src/Driver/Driver.java
@@ -1,13 +1,104 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.Driver to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Driver;
+
+import UserAccount.UserAccount;
 
 /**
  *
  * @author varun
  */
 public class Driver {
+    private String name;
+    private String ID;
+    private String username;
+    private String email;
+    private String address;
+    private String sex;
+    private String stateID;
+    private String age;
+    private UserAccount userAccount;
+
     
+    public Driver() {
+        
+    }
+    public Driver(String ID, String fname) {
+     this.ID = ID;
+     this.name = fname;
+    }
+
+    public UserAccount getUserAccount() {
+        return userAccount;
+    }
+
+    public void setUserAccount(UserAccount userAccount) {
+        this.userAccount = userAccount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getStateID() {
+        return stateID;
+    }
+
+    public void setStateID(String stateID) {
+        this.stateID = stateID;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
--- VaxSphere/src/Driver/DriverDirectory.java
+++ VaxSphere/src/Driver/DriverDirectory.java
@@ -4,10 +4,45 @@
  */
 package Driver;
 
+import Doctor.Doctor;
+import UserAccount.UserAccount;
+import java.util.ArrayList;
+import DatabaseConnection.*;
+
 /**
  *
  * @author varun
  */
 public class DriverDirectory {
+
+    private ArrayList<Driver> driverList;
     
+    public DriverDirectory() {
+        this.driverList = driverList;
+    }
+    
+    public void addDriver(Driver d){
+        driverList.add(d);
+    }
+    
+    public ArrayList<Driver> getDriverList() {
+        DataConn  dc = new DataConn();
+        driverList = dc.getDrivers();
+        return driverList;
+    }
+
+    public void setDriverList(ArrayList<Driver> driverList) {
+        this.driverList = driverList;
+    }
+    public DriverDirectory(ArrayList<Driver> driverList) {
+        this.driverList = driverList;
+    }
+    public Driver getDriverByUserAccount(UserAccount user) {
+        Driver customer = null;
+        for (Driver c: driverList) {
+            if (c.getUserAccount().equals(user))
+                customer = c;
+        }
+        return customer;
+    }
 }
