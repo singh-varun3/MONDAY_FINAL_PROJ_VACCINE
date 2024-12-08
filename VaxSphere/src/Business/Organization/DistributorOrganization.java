67015169764779a33a2a5e716ac5f6875ff2554a
Varun Singh <singh.varun3@northeastern.edu>
2024-12-08 17:00:29 -0500

    Modified logic for adding warehouse

--- VaxSphere/src/Business/Organization/DistributorOrganization.java
+++ VaxSphere/src/Business/Organization/DistributorOrganization.java
@@ -4,39 +4,54 @@
  * and open the template in the editor.
  */
 
-package Business.Organization;
+ package Business.Organization;
 
-import Business.Role.DistributorRole;
-import Business.Role.Role;
-import Business.Warehouse.WarehouseDirectory;
-import java.util.ArrayList;
-
-/**
- *
- * @author ethangomes
- */
-public class DistributorOrganization extends Organization{
-    
-    private WarehouseDirectory warehouseDirectory;
-    
-    public DistributorOrganization() {
-        super(OrganizationType.DistributorOrg.getValue());
-        warehouseDirectory = new WarehouseDirectory();
-    }
-
-    public WarehouseDirectory getWarehouseDir() {
-        return warehouseDirectory;
-    }
-    
-    
-    
-    
-    
-    @Override
-    public ArrayList<Role> getSupportedRole() {
-        ArrayList<Role> roles = new ArrayList<>();
-        roles.add(new DistributorRole());
-        return roles;
-    }
-    
-}
+ import Business.Role.DistributorRole;
+ import Business.Role.Role;
+ import Business.Warehouse.Warehouse;
+ import Business.Warehouse.WarehouseDirectory;
+ import java.util.ArrayList;
+ 
+ /**
+  *
+  * @author varun
+  */
+ public class DistributorOrganization extends Organization{
+     
+     private WarehouseDirectory warehouseDirectory;
+     
+     public DistributorOrganization() {
+         super(OrganizationType.DistributorOrg.getValue());
+         System.out.println("DistributorOrganization 1st");
+         warehouseDirectory = new WarehouseDirectory();
+         System.out.println("DistributorOrganization initialized, warehouseDirectory created.");
+         System.out.println("warehouseDirectory: " + warehouseDirectory.getWarehouseList());
+     }
+ 
+     public WarehouseDirectory getWarehouseDir() {
+         System.out.println("GET WAREHOUSE");
+ //        System.out.println("warehouseDirectory : " + warehouseDirectory.toString();
+         return warehouseDirectory;
+     }
+     
+     public WarehouseDirectory initializeDir(){
+         if(warehouseDirectory == null){
+             return warehouseDirectory = new WarehouseDirectory();
+         }else{
+             return warehouseDirectory;
+         }
+         
+     }
+ 
+     
+     
+     
+     @Override
+     public ArrayList<Role> getSupportedRole() {
+         ArrayList<Role> roles = new ArrayList<>();
+         roles.add(new DistributorRole());
+         return roles;
+     }
+     
+ }
+ 
--- VaxSphere/src/Business/Warehouse/WarehouseDirectory.java
+++ VaxSphere/src/Business/Warehouse/WarehouseDirectory.java
@@ -4,36 +4,50 @@
  * and open the template in the editor.
  */
 
-package Business.Warehouse;
+ package Business.Warehouse;
 
-import java.util.ArrayList;
-
-/**
- *
- * @author ethangomes
- */
-public class WarehouseDirectory {
-    
-    private ArrayList<Warehouse> warehouseList;
-
-    public WarehouseDirectory() {
-        warehouseList = new ArrayList<>();
-    }
-
-    public ArrayList<Warehouse> getWarehouseList() {
-        return warehouseList;
-    }
-    
-    public Warehouse addNewWareHouse(){
-        
-        Warehouse warehouse = new Warehouse();
-        warehouseList.add(warehouse);
-        return warehouse;
-    }
-    
-    
-    public void removeWarehouse(Warehouse warehouse){
-        
-        warehouseList.remove(warehouse);
-    }
-}
+ import java.util.ArrayList;
+ 
+ /**
+  *
+  * @author varun
+  */
+ public class WarehouseDirectory {
+     
+     private ArrayList<Warehouse> warehouseList;
+ 
+     public WarehouseDirectory() {
+         System.out.println("WarehouseDirectory 1st");
+         warehouseList = new ArrayList<Warehouse>();
+         System.out.println("WarehouseDirectory 2nd");
+     }
+ 
+     public ArrayList<Warehouse> getWarehouseList() {
+         return warehouseList;
+     }
+     
+     public Warehouse addNewWareHouse(){
+         
+         Warehouse warehouse = new Warehouse();
+         System.out.println("LINE 30 : " + warehouseList.toString());
+         warehouseList.add(warehouse);
+         return warehouse;
+     }
+     
+     public void initializeList(){
+         warehouseList = new ArrayList<Warehouse>();
+     }
+     
+     public void removeWarehouse(Warehouse warehouse){
+         
+         warehouseList.remove(warehouse);
+     }
+ 
+ //    @Override
+ //    public String toString() {
+ //        return "WarehouseDirectory{" + "warehouseList=" + warehouseList.toString() + '}';
+ //    }
+     
+     
+ }
+ 
